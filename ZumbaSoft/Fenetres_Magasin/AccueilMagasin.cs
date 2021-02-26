2531f49afc94edbda4b365c226f122d53a130af0
Victor Barrey <Vickybarrey@outlook.fr>
2021-02-26 21:14:28 +0100

    Ajout des formulaires vierges : Supprimer, Ajouter, Paramétrer, Authentification et Accueil.

---
+++ ZumbaSoft/Fenetres_Magasin/AccueilMagasin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZumbaSoft.Fenetres_Magasin
+{
+    public partial class AccueilMagasin : Form
+    {
+        public AccueilMagasin()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ZumbaSoft/Fenetres_Magasin/AjouterMagasin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZumbaSoft.Fenetres_Magasin
+{
+    public partial class AjouterMagasin : Form
+    {
+        public AjouterMagasin()
+        {
+            InitializeComponent();
+        }
+    }
+}
