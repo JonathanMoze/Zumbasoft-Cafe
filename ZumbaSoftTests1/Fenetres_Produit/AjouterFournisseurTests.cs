7543bece6964bfd302e50a7379334b06b66321a4
ninous mathilde <mathilde.ninous@etu.u-bordeaux.fr>
2021-04-01 22:49:53 +0200

    creation tests fenetres ventes

---
+++ ZumbaSoftTests1/Fenetres_Produit/AjouterFournisseurTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZumbaSoft.Fenetres_Produit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZumbaSoft.Fenetres_Produit.Tests
+{
+    [TestClass()]
+    public class AjouterFournisseurTests
+    {
+        [TestMethod()]
+        public void AjouterFournisseurTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void initObjectFournisseurTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void initItemsColorsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void fieldsIsValidTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void buttonOK_ClickTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
